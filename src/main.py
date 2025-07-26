eae1999d59f93a45c98c649b1ea13cb7e95312b6
objectiveSquid <magnuskzahle@outlook.com>
2025-07-26 22:29:22 +0200

    works nwo?

--- src/main.py
+++ src/main.py
@@ -17,7 +17,28 @@ def calculate():
     stoptime = request.form.get("starttime")
     hourly = request.form.get("hourly")
 
-    if starttime is None or stoptime is None:
-        return make_response("Missing starttime or stoptime", 400)
+    if starttime is None or stoptime is None or hourly is None:
+        return make_response("Missing parameters", 400)
 
-    return "TODO"
+    if starttime.count(":") != 1 or stoptime.count(":") != 1:
+        return make_response("Invalid time format", 400)
+
+    if hourly.isnumeric() is False:
+        return make_response("Hourly must be a number", 400)
+
+    start_hour, start_minute = starttime.split(":")
+    stop_hour, stop_minute = stoptime.split(":")
+
+    try:
+        start_time = int(start_hour) * 60 + int(start_minute)
+        stop_time = int(stop_hour) * 60 + int(stop_minute)
+    except ValueError:
+        return make_response("Invalid time format", 400)
+
+    if stop_time < start_time:
+        return make_response("Stop time must be after start time", 400)
+
+    hours_worked = (stop_time - start_time) / 60
+    total_pay = hours_worked * int(hourly)
+
+    return make_response(f"Total pay: {total_pay}", 200)
