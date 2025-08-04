364cb67387a6024c5bd0b20da331021411521162
objectiveSquid <magnuskzahle@outlook.com>
2025-08-04 16:13:48 +0200

    smol

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,11 @@ from main import app, disable_custom_redirect
 
 def main() -> None:
     disable_custom_redirect()
-    app.run(host="0.0.0.0", port=443, debug=True, ssl_context="adhoc")
+
+    try:
+        app.run(host="0.0.0.0", port=443, debug=True, ssl_context="adhoc")
+    except PermissionError:
+        app.run(host="0.0.0.0", port=4443, debug=True, ssl_context="adhoc")
 
 
 if __name__ == "__main__":
